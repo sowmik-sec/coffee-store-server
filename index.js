091ef2f329e0637665ca1156af3cc3bc1cbc5004
sowmik-sec <sowmik.sec@gmail.com>
2024-07-17 11:22:17 +0600

    delete user from db

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
